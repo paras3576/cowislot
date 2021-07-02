26fd20274b34ec2b0fcfc4e746d34621d496befd
Paras <paras3576@gmail.com>
2021-07-02 19:46:43 +0530

    Add reviews in slider and review box working

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.use(cors());
 
 
 app.get('/validEmail/:review_email', async (request, response) => {
-  const review_params = request.params.review_email.split(',');
+  const review_params = request.params.review_email.split(':');
   const review_email=review_params[0];
   const review=review_params[1];
   const star=review_params[2];
